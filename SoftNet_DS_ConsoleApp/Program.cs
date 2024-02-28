efe5895f0bf9014710e376f2397b9df6c347126d
yavuzgedik <yavuzgedik@outlook.com>
2024-02-28 18:48:21 +0300

    Add project files.

---
+++ SoftNet_DS_ConsoleApp/Program.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftNet_DS_ConsoleApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            string apiUrl = "{api_url}";
+
+            try
+            {
+                // JSON verisini oluşturun
+                var requestData = new
+                {
+                    Username = "{username}",
+                    Password = "{password}",
+                    Vkn = "{vkn}",
+                    Uuid = "{Uuid}"
+                };
+
+                // JSON verisini Serialize edin
+                // Newtonsoft kütüphanesi gereklidir.
+                string requestBody = JsonConvert.SerializeObject(requestData);
+
+                // İsteği oluşturun
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, apiUrl);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+                // POST isteği gönderin ve yanıtı alın
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                // Yanıtı kontrol edin
+                if (response.IsSuccessStatusCode)
+                {
+                    // Yanıt içeriğini okuyun
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    var responseModel = JsonConvert.DeserializeObject<ResponseVM>(responseContent);
+
+                    byte[] unzippedPdfData = Decompress(responseModel.Data.PdfData);
+
+                    string unzippedPdfFilePath = "unzipped_example.pdf";
+                    File.WriteAllBytes(unzippedPdfFilePath, unzippedPdfData);
+
+                    Console.WriteLine("PDF dosyası başarıyla ziplendi ve tekrar çözüldü.");
+                }
+                else
+                {
+                    Console.WriteLine("API isteği başarısız: " + response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("API bağlantısı sırasında hata oluştu: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+        public static byte[] Decompress(byte[] data)
+        {
+            using (MemoryStream compressedStream = new MemoryStream(data))
+            using (MemoryStream resultStream = new MemoryStream())
+            using (DeflateStream deflateStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
+            {
+                deflateStream.CopyTo(resultStream);
+                return resultStream.ToArray();
+            }
+        }
+    }
+
+    public class ResponseDispatch
+    {
+        public string UUID { get; set; }
+        public string XsltData { get; set; }
+        public byte[] PdfData { get; set; }
+    }
+
+    public class ResponseVM
+    {
+        public bool State { get; set; }
+        public string Message { get; set; }
+        public ResponseDispatch Data { get; set; }
+    }
+}
---
+++ SoftNet_WS_ConsoleApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using SoftNet_WS_ConsoleApp.SoftNetWS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftNet_WS_ConsoleApp
+{
+    internal class Program
+    {
+        static string username = "{username}";
+        static string password = "{password}";
+        static void Main(string[] args)
+        {
+            Integration10SoapClient client = new Integration10SoapClient();
+
+            var token = client.CreateUserToken(username, password, ModuleType.eDispatch);
+
+            Guid docId = Guid.Parse("{uuid}");
+
+            var disPDF = client.GetCommonDocumentPDFByUUIDs(token, new UbltrPDFInput[]
+            {
+                new UbltrPDFInput()
+                {
+                    UUID = docId,
+                    UseEmbeddedXSLT = true,
+                },
+            });
+
+            byte[] pdfData = disPDF.First().PDFContent;
+
+            byte[] zippedPdfData = Compress(pdfData);
+
+            string zipFilePath = "example.zip";
+            File.WriteAllBytes(zipFilePath, zippedPdfData);
+
+            byte[] unzippedPdfData = Decompress(zippedPdfData);
+
+            string unzippedPdfFilePath = "unzipped_example.pdf";
+            File.WriteAllBytes(unzippedPdfFilePath, unzippedPdfData);
+
+            Console.WriteLine("PDF dosyası başarıyla ziplendi ve tekrar çözüldü.");
+        }
+
+
+        public static byte[] Compress(byte[] data)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (DeflateStream deflateStream = new DeflateStream(memoryStream, CompressionMode.Compress))
+                {
+                    deflateStream.Write(data, 0, data.Length);
+                }
+                return memoryStream.ToArray();
+            }
+        }
+
+        public static byte[] Decompress(byte[] data)
+        {
+            using (MemoryStream compressedStream = new MemoryStream(data))
+            using (MemoryStream resultStream = new MemoryStream())
+            using (DeflateStream deflateStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
+            {
+                deflateStream.CopyTo(resultStream);
+                return resultStream.ToArray();
+            }
+        }
+
+       
+    }
+}
